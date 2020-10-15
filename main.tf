768595cc620740f18c11d763ee0ad453cd6ff110
Bryan Hoffman <bryan.hoffman@gmail.com>
2020-10-15 14:30:31 -0500

    initial k8s release

--- main.tf
+++ main.tf
@@ -26,3 +26,22 @@ module "aws-network" {
   private_subnet_a_cidr = "10.10.128.0/18"
   private_subnet_b_cidr = "10.10.192.0/18"
 }
+
+# EKS Configuration
+module "aws-kubernetes-cluster" {
+  source = "github.com/avator00/module-aws-kubernetes"
+
+  ms_namespace       = "microservices"
+  env_name           = local.env_name
+  aws_region         = local.aws_region
+  cluster_name       = local.k8s_cluster_name
+  vpc_id             = module.aws-network.vpc_id
+  cluster_subnet_ids = module.aws-network.subnet_ids
+
+  nodegroup_subnet_ids     = module.aws-network.private_subnet_ids
+  nodegroup_disk_size      = "20"
+  nodegroup_instance_types = ["t2.micro"]
+  nodegroup_desired_size   = 1
+  nodegroup_min_size       = 1
+  nodegroup_max_size       = 5
+}
