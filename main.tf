e60db19a99cad68e65d20ceaf8e479b24e5eec60
Bryan Hoffman <bryan.hoffman@gmail.com>
2020-10-15 15:26:35 -0500

    initial ArgoCD release 2

--- main.tf
+++ main.tf
@@ -54,6 +54,26 @@ module "aws-kubernetes-cluster" {
   nodegroup_max_size       = 5
 }
 
+# Create namespace
+# Use kubernetes provider to work with the kubernetes cluster API
+provider "kubernetes" {
+  load_config_file       = false
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.msur.certificate_authority.0.data)
+  host                   = data.aws_eks_cluster.msur.endpoint
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command     = "aws-iam-authenticator"
+    args        = ["token", "-i", "${data.aws_eks_cluster.msur.name}"]
+  }
+}
+
+# Create a namespace for microservice pods
+resource "kubernetes_namespace" "ms-namespace" {
+  metadata {
+    name = "microservices"
+  }
+}
+
 # GitOps Configuration
 module "argo-cd-server" {
   source = "github.com/avator00/module-argo-cd"
