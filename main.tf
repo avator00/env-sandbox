d02950c1fe7d491aaf66e3e1bb72ca2ea43589e8
Bryan Hoffman <bryan.hoffman@gmail.com>
2020-10-15 15:53:00 -0500

    initial ArgoCD release 3

--- main.tf
+++ main.tf
@@ -12,6 +12,11 @@ locals {
   k8s_cluster_name = "ms-cluster"
 }
 
+variable "mysql_password" {
+  type        = string
+  description = "Expected to be retrieved from environment variable TF_VAR_mysql_password"
+}
+
 provider "aws" {
   region = local.aws_region
 }
@@ -20,13 +25,12 @@ data "aws_eks_cluster" "msur" {
   name = module.aws-kubernetes-cluster.eks_cluster_id
 }
 
-# Network Configuration
 module "aws-network" {
-  source = "github.com/avator00/module-aws-network"
+  source = "github.com/implementing-microservices/module-aws-network"
 
   env_name              = local.env_name
   vpc_name              = "msur-VPC"
-  cluster-name          = local.k8s_cluster_name
+  cluster_name          = local.k8s_cluster_name
   aws_region            = local.aws_region
   main_vpc_cidr         = "10.10.0.0/16"
   public_subnet_a_cidr  = "10.10.0.0/18"
@@ -35,9 +39,8 @@ module "aws-network" {
   private_subnet_b_cidr = "10.10.192.0/18"
 }
 
-# EKS Configuration
 module "aws-kubernetes-cluster" {
-  source = "github.com/avator00/module-aws-kubernetes"
+  source = "github.com/implementing-microservices/module-aws-kubernetes"
 
   ms_namespace       = "microservices"
   env_name           = local.env_name
@@ -48,7 +51,7 @@ module "aws-kubernetes-cluster" {
 
   nodegroup_subnet_ids     = module.aws-network.private_subnet_ids
   nodegroup_disk_size      = "20"
-  nodegroup_instance_types = ["t2.micro"]
+  nodegroup_instance_types = ["t3.medium"]
   nodegroup_desired_size   = 1
   nodegroup_min_size       = 1
   nodegroup_max_size       = 5
@@ -74,9 +77,8 @@ resource "kubernetes_namespace" "ms-namespace" {
   }
 }
 
-# GitOps Configuration
 module "argo-cd-server" {
-  source = "github.com/avator00/module-argo-cd"
+  source = "github.com/implementing-microservices/module-argo-cd"
 
   aws_region            = local.aws_region
   kubernetes_cluster_id = data.aws_eks_cluster.msur.id
