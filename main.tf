89fb5fd737d4bb5ef50f4b76ee880a0c479b435a
Bryan Hoffman <bryan.hoffman@gmail.com>
2020-10-15 13:47:02 -0500

    initial network release

--- main.tf
+++ main.tf
@@ -8,6 +8,21 @@ terraform {
 
 locals {
   env_name         = "sandbox"
-  aws_region       = "<strong>region code</strong>"
+  aws_region       = "us-east-2"
   k8s_cluster_name = "ms-cluster"
+}
+
+# Network Configuration
+module "aws-network" {
+  source = "github.com/avator00/module-aws-network"
+
+  env_name              = local.env_name
+  vpc_name              = "msur-VPC"
+  cluster-name          = local.k8s_cluster_name
+  aws_region            = local.aws_region
+  main_vpc_cidr         = "10.10.0.0/16"
+  public_subnet_a_cidr  = "10.10.0.0/18"
+  public_subnet_b_cidr  = "10.10.64.0/18"
+  private_subnet_a_cidr = "10.10.128.0/18"
+  private_subnet_b_cidr = "10.10.192.0/18"
 }
